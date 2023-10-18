5285d1757a14eb37fc8533a145e9567656cc8e13
Alexandre Philibert <alexandre.philibert@ik.me>
2023-10-18 16:01:30 +0200

    Step 1 done

---
+++ src/main/java/org/example/Int.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Int {
+    private int value;
+
+    public Int() {
+        this.value = 0;
+    }
+
+    public Int(int value) {
+        this.value = value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,31 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] values = new int[args.length];
+
+        for (int i = 0; i < args.length; ++i) {
+            values[i] = parseInt(args[i]);
+        }
+
+        for (int v : values) {
+            System.out.println(v);
+        }
+    }
+
+    private static int parseInt(String s) {
+        int value = 0;
+        boolean negative = false;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (i == 0 && s.charAt(i) == '-') {
+                negative = true;
+            } else if (s.charAt(i) < '0' || s.charAt(i) > '9') {
+                throw new IllegalArgumentException("Oops");
+            } else {
+                value = value * 10 + s.charAt(i) - '0';
+            }
+        }
+
+        return negative ? -value : value;
     }
 }
